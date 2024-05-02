025c47206c20a9ffe7009115f6bcc76a6fb284fd
SUBHIN-TM <subhintm2018@gmail.com>
2024-05-02 18:32:10 +0530

    nav seperated the components

--- exmedia/src/App.jsx
+++ exmedia/src/App.jsx
@@ -1,49 +1,9 @@
-
-import './App.css'
-import './style.css'
-import { useRef } from "react";
-import { motion, sync, useCycle } from "framer-motion";
-import { useDimensions } from "./use-dimensions";
-import { MenuToggle } from "./assets/MenuToggle.jsx";
-import { Navigation } from "./assets/Navigation.jsx";
-
-const sidebar = {
-  open: (height = 1000) => ({
-    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
-    transition: {
-      type: "spring",
-      stiffness: 20,
-      restDelta: 2
-    }
-  }),
-  closed: {
-    clipPath: "circle(30px at 40px 40px)",
-    transition: {
-      delay: 0.5,
-      type: "spring",
-      stiffness: 400,
-      damping: 40
-    }
-  }
-};
-
+import Side from './assets/Components/SideNav.jsx'
 
 function App() {
-  const [isOpen, toggleOpen] = useCycle(false, true);
-  const containerRef = useRef(null);
-  const { height } = useDimensions(containerRef);
 
   return (
-    <motion.nav
-    initial={false}
-    animate={isOpen ? "open" : "closed"}
-    custom={height}
-    ref={containerRef}
-  >
-    <motion.div className="background" variants={sidebar} />
-    <Navigation />
-    <MenuToggle toggle={() => toggleOpen()} />
-     </motion.nav>
+    <Side />  
   )
 }
 
--- exmedia/src/assets/Rocket.jsx
+++
@@ -1,7 +0,0 @@
-import { Application } from '@splinetool/runtime';
-
-
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/f9tLQ151Ft51QV87/scene.splinecode');
