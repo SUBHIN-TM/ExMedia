798d93cd631e6651761ec17d86523ab76dfdc7ec
SUBHIN-TM <subhintm2018@gmail.com>
2024-05-02 18:09:49 +0530

    init

---
+++ exmedia/src/assets/Rocket.jsx
@@ -0,0 +1,7 @@
+import { Application } from '@splinetool/runtime';
+
+
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/f9tLQ151Ft51QV87/scene.splinecode');
