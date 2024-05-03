5d9bbc9303fcd3b5999446e984421ed39e465db7
SUBHIN-TM <subhintm2018@gmail.com>
2024-05-03 19:53:33 +0530

    padding for mobile devices modified

--- src/assets/Components/Details.jsx
+++ src/assets/Components/Details.jsx
@@ -43,7 +43,7 @@ const Details = () => {
 
             <div className='innerPara   bg-black'>
                 <div className='text-red-600 font-bold text-center text-6xl p-5 imageZoom'>WHO WE ARE</div>
-                <div className='px-20 text-justify text-lg'>
+                <div className='px-3 sm:px-20 text-justify text-lg'>
                     <p className='text-white'>Since 2006, Exmedia has been a trailblazing Advertisement agency, driving brand success through innovation, creativity, and strategic excellence. With an illustrious history, we've honed our craft to deliver impactful campaigns that transcend conventional boundaries. From dynamic visuals to compelling narratives, our diverse expertise in advertising, design, and branding has fueled the growth of numerous clients. We're not just an agency; we're your partners in shaping brand legacies. With a deep understanding of market dynamics, consumer behavior, and emerging trends, Exmedia is your compass to navigate the ever-evolving advertising landscape, ensuring your brand remains at the forefront of industry innovation.</p><br />
                     <p className=  ' text-white pb-8'>Based on our strategy work, we create distinctive brand Your Ultimate Advertisement Company, where innovation meets creativity to redefine your brand's journey. Our agency stands as a pillar of excellence, providing a comprehensive suite of services including Studio, Graphics, and Branding. Step into our cutting-edge studio, a hub of ingenuity where ideas take shape. From brainstorming sessions to concept realization, our team collaborates to bring your vision to life. Our studio is the birthplace of dynamic campaigns that captivate and resonate.</p>
                 </div>
@@ -61,7 +61,7 @@ const Details = () => {
 
             <div className=' grid md:flex  bg-black lg:bg-white'>
                 <div className='text-white w-3/4  p-5 imageZoom1'  > <img src={studio} alt="" />  </div>
-                <div className='text-red-600   pt-2 bg-black p-5  w-9/12 '>
+                <div className='text-red-600   pt-2 bg-black p-5 sm:w-9/12 '>
                     <div className='  p-2 text-red-500 font-bold text-2xl'>STUDIO</div>
                     <div className='  pl-2 text-red-500 font-mono text-xl'>ELEVATE YOUR IMPACTS & CONNECT BRANDS WITH CUSTOMERS.</div>
                     <div className=' mb-8 pl-2 pt-3 text-white  text-lg text-justify'>Where Creativity Finds Its Canvas. Our Studio Is A Haven For Innovative Ideas, Bringing Visions To Life With Precision And Passion. With State-Of-The-Art Equipment And A Team Of Skilled Professionals, We Craft Captivating Visuals That Tell Compelling Stories. Whether It's Photography, Videography, Or Audio Production, Our Studio Is Equipped To Deliver Excellence In Every Frame And Note. From Concept To Execution, We Transform Concepts Into Breathtaking Realities That Leave Lasting Impressions. At Exmedia, Creativity Knows No Bounds, And Our Studio Can Convert Imagination Takes It To Another Unexpected Level. Step Into A World Of Boundless Possibilities And Let Your Brand's Story Unfold In Stunning Visual Brilliance.</div>
@@ -81,7 +81,7 @@ const Details = () => {
 
             <div className=' grid md:flex    bg-black lg:bg-white'>
                 <div className= 'text-white w-3/4  p-5 imageZoom2'  > <img src={robotic} alt="" />  </div>
-                <div className='text-red-600   pt-2 bg-black p-5  w-9/12 '>
+                <div className='text-red-600   pt-2 bg-black p-5  sm:w-9/12 '>
                     <div className='  p-2 text-red-500 font-bold text-2xl'>GRAPHICS</div>
                     <div className='  pl-2 text-red-500 font-mono text-xl'>DESIGNS THAT INSPIRE, CONNECT,AND LEAVE A LASTING IMPRESSION</div>
                     <div className=' mb-8 pl-2 pt-3 text-white  text-lg text-justify'>Introducing Exmedia Graphic Design, where imagination meets design prowess. Our Graphic studio is a playground for innovative ideas, transforming concepts into visually stunning realities. With a skilled team of designers, we craft graphics that captivate and communicate. From logos to intricate designs, our expertise spans a spectrum of visual elements. We merge artistry with strategy, ensuring each design aligns seamlessly with your brand's essence. At Exmedia, we understand the power of visuals to leave indelible impressions. Step into a world of creativity, where every pixel tells a story. Elevate your brand with Exmedia Graphic Design and make your visual identity a masterpiece.</div>
@@ -101,7 +101,7 @@ const Details = () => {
 
             <div className=' grid md:flex   bg-black lg:bg-white'>
                 <div className='imageZoom text-white w-3/4  p-5'  > <img src={fingers} alt="" />  </div>
-                <div className='text-red-600   pt-2 bg-black p-5  w-9/12 '>
+                <div className='text-red-600   pt-2 bg-black p-5 sm:w-9/12 '>
                     <div className='  p-2 text-red-500 font-bold text-2xl'>BRANDING</div>
                     <div className='  pl-2 text-red-500 font-mono text-xl'>YOUR BRAND, YOUR STORY : WE SHAPE IDENTITIES WITH IMPACT</div>
                     <div className=' mb-8 pl-2 pt-3 text-white  text-lg text-justify'>Welcome to Exmedia Branding, where identities come to life. Our expertise lies in shaping brands that resonate deeply and endure. With a keen understanding of market dynamics, we craft strategic brand identities that leave lasting imprints. From logos to comprehensive brand strategies, our team blends creativity with market insights to create identities that stand out. We believe in the power of authentic storytelling and meaningful connections. At Exmedia, we architect legacies. Step into a world of brand transformation, where every element speaks volumes, and consider all updated strategies to build and ensure the trust of your brand in the respective community or customer. Choose Exmedia Branding to make your mark and forge enduring relationships with your Valuable customer & audience.</div>
